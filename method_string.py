943c39b300070cb99ec8961d28e1a99338c5723e
fasih-kode <55391942+fasih-kode@users.noreply.github.com>
2022-09-13 07:29:09 +0000

    Ok

--- method_string.py
+++ method_string.py
@@ -8,11 +8,12 @@ print(belajar.find('z'))
 # -1 artinya karakter itu tidak ada
 print(belajar.find('pertama'))
 # kata 'pertama' ada di urutan ke-8
-print(belajar.replace('baik' , 'bagus'))
-print(belajar.replace('Baik' , 'bagus'))
+print(belajar.replace('baik', 'bagus'))
+print(belajar.replace('Baik', 'bagus'))
 # karena kata 'Baik' bukan value, maka outputnya sama seperti aslinya.
-print(belajar.replace('l' , 'r'))
+print(belajar.replace('l', 'r'))
 # mengganti karakter dengan karakter lain
 print('baik' in belajar)
 # ini adalah bentuk true atau false dengan 'in'
 print('laik' in belajar)
+#coba dulu
