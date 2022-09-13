8b8d8eb753b9c51f05beddfb0912845b19dd9503
fasih-kode <55391942+fasih-kode@users.noreply.github.com>
2022-09-13 13:32:38 +0700

    Create belajar_string.py
    Mulai

---
+++ belajar_string.py
@@ -0,0 +1,22 @@
+kata = 'Ini adalah kata Mutiara'
+print(kata)
+
+puisi = 'Ini adalah kata "Mutiara"'
+print(puisi)
+
+lagu = "Ini adalah kata 'Mutiara'"
+print(lagu)
+
+bijak = '''Ini adalah kata Mutiara
+yang sama sekali
+tidak ada mutiara-mutiaranya'''
+print(bijak)
+
+celana = 'Ini adalah kata Mutiara'
+print(celana[0])
+print(celana[0:8])
+print(celana[0:-1])
+# yang ada dalam bracket ini indexing
+#Ini adalah kata Mutiara
+#012 345678
+#baris dengan -1 dihitung dari huruf terakhir kata
